dab23aba81c659de8a9aad25977b8edc48dceded
Jardel <jardelgoncalves1996@gmail.com>
2019-09-28 17:35:45 -0300

    config do app

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import knex from 'knex'
+import { Model } from 'objection'
+
+import routes from './routes'
+import knexConfig from '../knexfile'
+
+const app = express()
+
+// config models objection.js
+const Knex = knex(knexConfig[process.env.NODE_ENV || 'development'])
+Model.knex(Knex)
+
+// middlewares
+app.use(express.json())
+
+// routes
+app.use(routes)
+
+export default app
