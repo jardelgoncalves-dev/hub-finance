7911abf8e0083a4f62d2c9c8b07c4390d977b3c1
Jardel <jardelgoncalves1996@gmail.com>
2019-09-28 12:00:36 -0300

    renoemando de profit para income

--- backend/src/database/migrations/20190927215113_income.js
+++ backend/src/database/migrations/20190927215113_income.js
@@ -1,15 +1,17 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('profits', table => {
+  return knex.schema.createTable('incomes', table => {
     table.increments()
-    table.string('name')
+    table.string('description')
     table.decimal('value')
     table.integer('user_id').unsigned()
     table.foreign('user_id').references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
+    table.integer('category_id').unsigned()
+    table.foreign('category_id').references('id').inTable('categories').onUpdate('CASCADE').onDelete('CASCADE')
     table.timestamps()
   })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('profits')
+  return knex.schema.dropTable('incomes')
 };
