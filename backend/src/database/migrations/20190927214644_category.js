5de9dccc2070dff86a33f2aab424855f4f1a3e52
Jardel <jardelgoncalves1996@gmail.com>
2019-09-28 12:04:36 -0300

    add novas colunas para manipular corretamente as categorias de cada lançamento

--- backend/src/database/migrations/20190927214644_category.js
+++ backend/src/database/migrations/20190927214644_category.js
@@ -3,6 +3,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('categories', table => {
     table.increments()
     table.string('name')
+    table.boolean('income')
+    table.boolean('expense')
     table.timestamps()
   })
 };
