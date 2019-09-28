46acd769b3dba7746782531bd562ed34905cf337
Jardel <jardelgoncalves1996@gmail.com>
2019-09-28 12:05:17 -0300

    add seeds de category

--- backend/src/database/seeds/01_category.js
+++ backend/src/database/seeds/01_category.js
@@ -3,17 +3,21 @@ exports.seed = function(knex) {
   return knex('categories').del()
     .then(function () {
       return knex('categories').insert([
-        { id: 1, name: 'Educação', created_at: 'now()', updated_at: 'now()' },
-        { id: 2, name: 'Eletrônicos', created_at: 'now()', updated_at: 'now()' },
-        { id: 3, name: 'Lazer', created_at: 'now()', updated_at: 'now()' },
-        { id: 4, name: 'Outros', created_at: 'now()', updated_at: 'now()' },
-        { id: 5, name: 'Restaurante', created_at: 'now()', updated_at: 'now()' },
-        { id: 6, name: 'Saúde', created_at: 'now()', updated_at: 'now()' },
-        { id: 7, name: 'Serviços', created_at: 'now()', updated_at: 'now()' },
-        { id: 8, name: 'Supermercado', created_at: 'now()', updated_at: 'now()' },
-        { id: 9, name: 'Trasnporte', created_at: 'now()', updated_at: 'now()' },
-        { id: 10, name: 'Vestuário', created_at: 'now()', updated_at: 'now()' },
-        { id: 11, name: 'Viagem', created_at: 'now()', updated_at: 'now()' }
+        { id: 1, name: 'Educação', income: false, expense: true },
+        { id: 2, name: 'Eletrônicos', income: false, expense: true },
+        { id: 3, name: 'Lazer', income: false, expense: true },
+        { id: 4, name: 'Outros', income: true, expense: true },
+        { id: 5, name: 'Restaurante', income: false, expense: true },
+        { id: 6, name: 'Saúde', income: false, expense: true },
+        { id: 7, name: 'Serviços', income: false, expense: true },
+        { id: 8, name: 'Supermercado', income: false, expense: true },
+        { id: 9, name: 'Trasnporte', income: false, expense: true },
+        { id: 10, name: 'Vestuário', income: false, expense: true },
+        { id: 11, name: 'Presente', income: true, expense: false },
+        { id: 12, name: 'Investimento', income: true, expense: false },
+        { id: 13, name: 'Recompensa', income: true, expense: false },
+        { id: 14, name: 'Salário', income: true, expense: false }
+
       ]);
     });
 };
