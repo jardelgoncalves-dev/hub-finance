1009c1235110becc67a169b4dd180bd5bbc7166d
Jardel <jardelgoncalves1996@gmail.com>
2019-09-28 12:03:25 -0300

    add jwt e APP_SECRET global nos tests

--- backend/test/integration/helpers.js
+++ backend/test/integration/helpers.js
@@ -1,7 +1,13 @@
 import { expect } from 'chai'
 import supertest from 'supertest'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
 import app from '../../src/app'
+
 
 global.app = app
 global.request = supertest(app)
 global.expect = expect
+global.jwt = jwt
+global.APP_SECRET = process.env.APP_SECRET
