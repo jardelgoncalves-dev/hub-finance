486485d9471c7578e3d0259bb538bbd30dde2dbe
Jardel <jardelgoncalves1996@gmail.com>
2019-09-29 17:27:15 -0300

    estilo global da aplicação

---
+++ frontend/src/components/GlobalStyle/index.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+
+import bg from '../../assets/bg_big.svg'
+import small from '../../assets/bg_small.svg'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    width: 100%;
+    height: 120px;
+    background-repeat: no-repeat;
+    background-image: url(${bg});
+    background-color: #F6F6F4;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+
+  html,
+  body,
+  input,
+  button {
+    font-family: sans-serif;
+  }
+
+  h3 {
+    font-size: 30px;
+    color: #FFF;
+  }
+
+  label {
+    color: #5A5A5A
+  }
+`;
+
+export const ChangeBackground = createGlobalStyle`
+  body {
+    background-image: url(${small});
+  }
+`
