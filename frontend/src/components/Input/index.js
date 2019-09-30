8e4645ee054a86a004cebd361e5a6c16a43e210c
Jardel <jardelgoncalves1996@gmail.com>
2019-09-30 08:46:49 -0300

    alterando componente de input para receber mensagem de erro

--- frontend/src/components/Input/index.js
+++ frontend/src/components/Input/index.js
@@ -1,19 +1,43 @@
+import React from 'react'
 import styled from 'styled-components';
 
-const Input = styled.input`
-  display: block;
-  padding: 14px;
-  border: none;
-  border-radius: 5px;
-  color: #F2691B;
-  font-size: 16px;
-  background-color: #FFF;
-  width: 100%;
+const StyleInput = styled.div`
   margin: 32px 0;
-  box-shadow: 0px 0px 45px -12px rgba(0,0,0,0.2);
-  ::placeholder {
-    color: #D69875;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  input {
+    display: block;
+    padding: 14px;
+    border: none;
+    border-radius: 5px;
+    color: #F2691B;
+    font-size: 16px;
+    background-color: #FFF;
+    width: 100%;
+    box-shadow: 0px 0px 45px -12px rgba(0,0,0,0.2);
+    ::placeholder {
+      color: #D69875;
+    }
+  }
+  small.errorMessage {
+    font-size: 14px;
+    color: #960000
   }
 `;
+
+const Input = ({ placeholder, type, onChange, errorMessage, style }) => (
+  <StyleInput>
+    <input
+      placeholder={placeholder}
+      type={type}
+      onChange={onChange}
+      style={style}
+    />
+    { errorMessage && <small className="errorMessage">{ errorMessage }</small> }
+    
+  </StyleInput>
+)
 
 export default Input
