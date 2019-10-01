ac8f9bd8f43bb4992b4891e315188e634711a1d4
Jardel <jardelgoncalves1996@gmail.com>
2019-10-01 00:06:41 -0300

    realizando cadastro de usuario (e login automatico) com redux

--- frontend/src/pages/Cadastro/index.js
+++ frontend/src/pages/Cadastro/index.js
@@ -1,14 +1,78 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { bindActionCreators } from 'redux'
 
 import Header from '../../components/Header'
 import Avatar from '../../components/Avatar'
 import Container from '../../components/Container'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
+import Alert from '../../components/Alert'
+
 import logo from '../../assets/logo_white.svg'
 import userIcon from '../../assets/user.svg'
 
+import { isEmail, isRequired } from '../../util/validator'
+import * as UserActions from '../../store/actions/user'
+
+
 class Cadastro extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    error_name: '',
+    error_email: '',
+    error_password: '',
+    error: '',
+    stop_request: false
+  }
+
+  componentDidMount () {
+    this.props.logout()
+  }
+
+  handleInputChange = (name, value) => {
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleValidateField = () => {
+    this.setState({ error_email: '', error_name: '', error_password: '', stop_request: false })
+    if (!isEmail(this.state.email)){
+      this.setState({ error_email: 'Email inválido', stop_request: true })
+    }
+    if (!isRequired(this.state.password) ) {
+      this.setState({ error_password: 'Este campo é obrigatório', stop_request: true })
+    }
+    if (!isRequired(this.state.name) ) {
+      this.setState({ error_name: 'Este campo é obrigatório', stop_request: true })
+    }
+    if (!isRequired(this.state.email)){
+      this.setState({ error_email: 'Este campo é obrigatório', stop_request: true })
+    }
+  }
+
+  handleSubmit = async () => {
+    await this.handleValidateField()
+    if (!this.state.stop_request) {
+      const { name, email, password } = this.state
+      const response = await this.props.register({ name, email, password })
+      if (response.status === 201) {
+        this.setState({ error: '' })
+        this.props.history.push('/home')
+      } else {
+        if(response.data.error && response.data.error.message) {
+          this.setState({ error: response.data.error.message })
+        }
+        else if(response.data.error && response.data.error.email) {
+          this.setState({ error_email: response.data.error.email })
+        }
+      }
+    }
+  }
+
   render () {
     return (
       <div>
@@ -16,7 +80,7 @@ class Cadastro extends Component {
           logo={logo}
           links={[
             { url: '/', name: 'Login' },
-            { url: '/', name: 'Cadastro' },
+            { url: '/cadastro', name: 'Cadastro' },
           ]}
           buttons={[<Button key="1" small white>Sair</Button>]}
         >
@@ -32,10 +96,27 @@ class Cadastro extends Component {
             width: '600px',
             marginTop: '16px'
           }}>
-            <Input placeholder="Nome" />
-            <Input placeholder="Email" />
-            <Input placeholder="Password" />
-            <Button>Finalizar</Button>
+            { this.state.error.length !== 0 && <Alert danger>{ this.state.error }</Alert> }
+            <Input
+              placeholder="Nome"
+              onChange={(event) => this.handleInputChange('name', event.target.value)}
+              errorMessage={this.state.error_name}
+              style={{ marginBottom: '32px', marginTop: '32px' }}
+            />
+            <Input
+              placeholder="Email"
+              onChange={(event) => this.handleInputChange('email', event.target.value)}
+              errorMessage={this.state.error_email}
+              style={{ marginBottom: '32px' }}
+            />
+            <Input
+              placeholder="Password"
+              type="password"
+              onChange={(event) => this.handleInputChange('password', event.target.value)}
+              errorMessage={this.state.error_password}
+              style={{ marginBottom: '32px' }}
+            />
+            <Button onClick={this.handleSubmit}>Finalizar</Button>
           </div>
         </Container>
       </div>
@@ -43,4 +124,12 @@ class Cadastro extends Component {
   }
 }
 
-export default Cadastro;
+const mapStateToProps = state => ({
+  auth: state.user.isAuthenticated,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(UserActions, dispatch);
+
+
+export default  connect(mapStateToProps, mapDispatchToProps)(Cadastro) ;
