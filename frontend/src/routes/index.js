42c446335d1f41c71d281d2c8ba9dffbf6a1a8fc
Jardel <jardelgoncalves1996@gmail.com>
2019-10-02 00:13:16 -0300

    add rotas

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -5,6 +5,9 @@ import { ConnectedRouter } from 'connected-react-router'
 import Login from '../pages/Login';
 import Cadastro from '../pages/Cadastro';
 import Home from '../pages/Home';
+import AddIncome from '../pages/AddIncome';
+import AddExpense from '../pages/AddExpense';
+import Listar from '../pages/Listar';
 
 import history from './history'
 import ProtectedRoute from '../ProtectedRoute'
@@ -14,7 +17,10 @@ const Routes = () => (
     <Switch>
       <Route exact path="/" component={Login} />
       <Route path="/cadastro" component={Cadastro} />
-      <Route path="/home" component={ProtectedRoute(Home)} />
+      <Route exact path="/home" component={ProtectedRoute(Home)} />
+      <Route path="/home/add_income" component={ProtectedRoute(AddIncome)} />
+      <Route path="/home/add_expense" component={ProtectedRoute(AddExpense)} />
+      <Route path="/home/listar" component={ProtectedRoute(Listar)} />
     </Switch> 
   </ConnectedRouter>
 )
