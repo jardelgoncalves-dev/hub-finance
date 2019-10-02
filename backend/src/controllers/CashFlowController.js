cc61f7303a94daa7218ac279647840172fbc5d35
Jardel <jardelgoncalves1996@gmail.com>
2019-10-01 23:33:39 -0300

    add metodo que retorna o total em despesa e em receitas

--- backend/src/controllers/CashFlowController.js
+++ backend/src/controllers/CashFlowController.js
@@ -1,14 +1,33 @@
 import ViewCashFlow from '../models/ViewCashFlowBalance'
+import CashFlow from '../models/CashFlow'
+
 
 
 export default class CashFlowController {
-  static async index (req, res) {
+  static async balance (req, res) {
     try {
       const balances = await ViewCashFlow.query()
                                          .where('user_id', req.userId)
                                          .orderBy('month', 'DESC')
                                          .limit(12)
       return res.status(200).json(balances)
+    } catch (err) {
+      return res.status(501).json({ message: 'Ocorreu um erro ao buscar resumo dos balanços' })
+    }
+  }
+
+  static async balanceByFlowType (req, res) {
+    try {
+      
+      const expense = await CashFlow.query()
+                              .sum({ expense: 'value' })
+                              .where('user_id', req.userId)
+                              .where('flow_type', 'expense')
+      const income = await CashFlow.query()
+                              .sum({ income: 'value' })
+                              .where('user_id', req.userId)
+                              .where('flow_type', 'income')
+      return res.status(200).json({ expense: expense[0].expense, income: income[0].income })
     } catch (err) {
       return res.status(501).json({ message: 'Ocorreu um erro ao buscar resumo dos balanços' })
     }
