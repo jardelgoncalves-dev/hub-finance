18e131e8d9aad303bee983031c1169ad70ed6a6a
Jardel <jardelgoncalves1996@gmail.com>
2019-10-02 00:12:41 -0300

    adicionar funcionalidade de pesquisar por data

--- frontend/src/pages/Listar/index.js
+++ frontend/src/pages/Listar/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker'
-import "../../components/react-datepicker/dist/react-datepicker.css"
+import { connect } from "react-redux";
+import { bindActionCreators } from 'redux'
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 import { ChangeBackground } from '../../components/GlobalStyle'
 import Header from '../../components/Header'
@@ -10,46 +12,97 @@ import Button from '../../components/Button'
 import Row from '../../components/Row'
 import { CardExpense } from '../../components/Card'
 
-
 import logo from '../../assets/logo_white.svg'
 import userIcon from '../../assets/user.svg'
+import "../../components/react-datepicker/dist/react-datepicker.css"
+import 'react-notifications/lib/notifications.css';
 
+import api from '../../services/api'
+import * as UserActions from '../../store/actions/user'
 
 class Listar extends Component {
+
+  state = {
+    date: new Date(),
+    expenses: [],
+    search: false
+  }
+
+  handleDate = () => {
+    const year = this.state.date.getFullYear()
+    const month = this.state.date.getMonth() + 1
+    api.get(`/expenses/${year}/${month}/period`)
+    .then(response => {
+      this.setState({ expenses: response.data, search: true })
+    }).catch(err => {
+      NotificationManager.error(null, 'Ocorreu um erro! Tente novamente');      
+    })
+  }
+
   render () {
     return (
       <div>
         <Header
           logo={logo}
           links={[
-            { url: '/', name: 'Página Principal' },
+            { url: '/home', name: 'Página Principal' },
           ]}
-          buttons={[<Button key="1" small white>Sair</Button>]}
+          buttons={[
+            <Button
+            key="1"
+            small
+            white
+            onClick={() => this.props.logout()}
+          >
+            Sair
+          </Button>
+          ]}
         >
           <img src={logo} alt="Logo" />
           <Avatar
             image={userIcon}
-            text="Jardel Gonçalves"
+            text={this.props.user.name}
           />
         </Header>
         <Container center column>
-        <Row>
-        <DatePicker
-          selected={new Date()}
-          onChange={() => {}}
-          dateFormat="MM/yyyy"
-          showMonthYearPicker
-        />
-        <Button>Pesquisar</Button>
-        </Row>
-          <CardExpense />
-          <CardExpense />
-          <CardExpense />
+        <h4 style={{ color: '#FFF', marginBottom: '20px' }}>Pesquise as despesas por Mês e Ano</h4>
+          <Row>
+            <DatePicker
+              selected={this.state.date}
+              onChange={(date) => this.setState({ date: date }) }
+              dateFormat="MM/yyyy"
+              showMonthYearPicker
+            />
+            <Button onClick={this.handleDate} >Pesquisar</Button>
+          </Row>
+          { !this.state.expenses.length && this.state.search && (
+            <p style={{ color: '#6f6f6f', marginBottom: '50px', marginTop: '50px' }}>
+              Nenhuma despesa foi encontrado para a data informada
+            </p>
+          )}
+          { this.state.expenses.map(expense => (
+              <CardExpense
+                key={expense.id}
+                value={expense.value}
+                color={expense.categories.color}
+                category={expense.categories.name}
+                description={expense.description}
+              />
+            )) }
         </Container>
+        <NotificationContainer />
         <ChangeBackground />
       </div>
     )
   }
 }
 
-export default Listar;
+const mapStateToProps = state => ({
+  auth: state.user.isAuthenticated,
+  user: state.user.user
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(UserActions, dispatch);
+
+export default  connect(mapStateToProps, mapDispatchToProps)(Listar);
